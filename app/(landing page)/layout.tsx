aebea001c4afb76f77d594bfd01bac317655040c
Abdulaziz <abdulazizyusupaliev009@gmail.com>
2026-02-12 23:48:03 +0500

    correction

--- app/(landing page)/layout.tsx
+++ app/(landing page)/layout.tsx
@@ -10,12 +10,12 @@ export default function LandingPageLayout({children}: {children: React.ReactNode
                 className="pointer-events-none"
                 topColor="#5227FF"
                 bottomColor="#FF9FFC"
-                intensity={1}
+                intensity={8}
                 rotationSpeed={0.4}
                 glowAmount={0.0002}
                 pillarWidth={7}
-                pillarHeight={0.4}
-                noiseIntensity={0.5}
+                pillarHeight={0.7}
+                noiseIntensity={0}
                 pillarRotation={20}
                 interactive={false}
                 mixBlendMode="screen"
