c472bccfddacd512eb5a6f3702d880348ed38eca
Abdulaziz <abdulazizyusupaliev009@gmail.com>
2026-02-27 21:57:39 +0500

    auth merge

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,9 +1,16 @@
+"use client"
+import { logout } from '@/app/(authentication)/lib/actions'
+import Button from '@/components/ui/Button'
 import React from 'react'
+
 
 const Dashboard = () => {
     return (
-        <div className="w-full h-[100vh] flex justify-center items-center">
-            <h1 className="text-4xl font-bold">Dashboard</h1>
+        <div className="container mx-auto h-[100vh] flex justify-center items-center max-[644px]:w-[80%] ">
+            <div className="flex flex-col gap-4 items-center">
+                <h1 className="text-4xl font-bold text-center max-[644px]:text-[20px]">Thank you for signing up, new features will be coming soon!</h1>
+                <Button className='w-[100px]' onClick={logout}>Logout</Button>
+            </div>
         </div>
     )
 }
