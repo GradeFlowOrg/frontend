21124c2d79d63806ad6a8b32ce97e34bc4a4b91d
Abdulaziz <abdulazizyusupaliev009@gmail.com>
2026-03-01 18:52:14 +0500

    input fix

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <input
         ref={ref}
-        className={`rounded-lg border border-black/10 bg-white px-3 py-2 text-sm outline-none transition focus:border-[#0046FF] dark:border-white/10 dark:bg-[#0b1220] dark:text-white ${className || ""}`}
+        className={`rounded-xl border border-black/10 bg-white px-4 py-3 text-lg md:text-base outline-none transition focus:border-[#0046FF] dark:border-white/10 dark:bg-[#0b1220] dark:text-white ${className || ""}`}
         {...props}
       />
     );
